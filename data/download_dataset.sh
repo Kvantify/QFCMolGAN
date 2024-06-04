3aa4fc8dc056bac252b94b8aa907146b6a7d9767
Thomas Bækkegaard <104205764+thomasbaekkegaard@users.noreply.github.com>
2024-06-04 14:56:17 +0200

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -5,3 +5,7 @@ rm gdb9.tar.gz
 
 wget https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
 wget https://github.com/gablg1/ORGAN/raw/master/organ/SA_score.pkl.gz
+
+wget https://raw.githubusercontent.com/gablg1/ORGAN/master/data/qm9_5k.csv
+tail -n +2 "qm9_5k.csv" > "qm9_5k.smi"
+rm "qm9_5k.csv"
